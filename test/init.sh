c2df2ba0ff72a07db85c40e763455e479c76be24
steve-chavez <stevechavezast@gmail.com>
2025-10-13 09:57:17 -0500

    rep

--- test/init.sh
+++ test/init.sh
@@ -19,3 +19,29 @@ echo 'create extension citext;' > "$TMPDIR/extension-custom-scripts/autoinc/afte
 mkdir -p "$TMPDIR/extension-custom-scripts/fuzzystrmatch"
 echo 'create table t1();' > "$TMPDIR/extension-custom-scripts/fuzzystrmatch/before-create.sql"
 echo 'drop table t1; create table t2 as values (1);' > "$TMPDIR/extension-custom-scripts/fuzzystrmatch/after-create.sql"
+
+# assert both before-create and after-create scripts are run
+mkdir -p "$TMPDIR/extension-custom-scripts/postgres_fdw"
+cat > "$TMPDIR/extension-custom-scripts/postgres_fdw/after-create.sql" <<'EOF'
+do $$
+declare
+  is_super boolean;
+begin
+  is_super = (
+    select usesuper
+    from pg_user
+    where usename = 'privileged_role'
+  );
+
+  -- Need to be superuser to own FDWs, so we temporarily make privileged_role superuser.
+  if not is_super then
+    alter role privileged_role superuser;
+  end if;
+
+  alter foreign data wrapper postgres_fdw owner to privileged_role;
+
+  if not is_super then
+    alter role privileged_role nosuperuser;
+  end if;
+end $$;
+EOF
